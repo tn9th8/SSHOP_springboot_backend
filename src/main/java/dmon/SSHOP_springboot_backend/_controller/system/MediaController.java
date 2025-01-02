41e4e8ad9d2b35035c2ee6a951ffd97751ba22d4
tn9th8 <tn9th8@gmail.com>
2025-01-02 10:25:51 +0700

    chore: add document

--- src/main/java/dmon/SSHOP_springboot_backend/_controller/system/MediaController.java
+++ src/main/java/dmon/SSHOP_springboot_backend/_controller/system/MediaController.java
@@ -2,18 +2,16 @@ package dmon.SSHOP_springboot_backend._controller.system;
 
 import dmon.SSHOP_springboot_backend._service.system.IMediaService;
 import dmon.SSHOP_springboot_backend.dto.response.system.UploadRes;
-import dmon.SSHOP_springboot_backend.utils.annotation.ApiMessage;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.time.Instant;
 
 public class MediaController {
     @RestController
